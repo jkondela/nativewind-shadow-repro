75d28e1d04eebada3fa0d55ca545cd3c256f48db
Jaro Kondela <jaqkit@hotmail.sk>
2025-02-25 13:45:39 +0100

    Fix typo

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -17,7 +17,7 @@ export const ScreenContent = () => {
     <View className="flex-1 items-center justify-center bg-gray-100">
       {/* With "shadow-lg" */}
       <View className="mb-12">
-        <Text className="mb-4 text-2xl font-bold">With shadow-lg (blue background is working)</Text>
+        <Text className="mb-4 text-2xl font-bold">With shadow-lg (blue background missing)</Text>
         <StyledThirdPartyComponent
           className="bg-blue-500 p-4"
           containerClassname="border border-grey-200 bg-white shadow-lg shadow-grey-300"
@@ -25,7 +25,9 @@ export const ScreenContent = () => {
       </View>
       {/* Without "shadow-lg" */}
       <View className="mb-12">
-        <Text className="mb-4 text-2xl font-bold">Without shadow-lg (blue background missing)</Text>
+        <Text className="mb-4 text-2xl font-bold">
+          Without shadow-lg (blue background is working)
+        </Text>
         <StyledThirdPartyComponent
           className="bg-blue-500 p-4"
           containerClassname="border border-grey-200 bg-white shadow-grey-300"
